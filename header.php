c755f620e568ccb5225b11e8f7a50c11d0e20087
danish <ahmaddanish040121@gmail.com>
2024-06-09 04:04:40 +0800

    Describe your changes here

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
             <ul id="nav-links">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="aboutus.php">About Us</a></li>
-                <li><a href="#" id="donate-link">Donate</a></li>
+                <li><a href="donor.php">Donate</a></li>
                 <li><a href="#" id="volunteer-link">Volunteers</a></li>
                 <li><a href="#">News & Updates</a></li>
                 <li><a href="mailto:imran24022004@gmail.com?subject=Contact Us Inquiry">Contact Us</a></li>
