d66995f32410d033bf658ea94cf9fc46259f0516
Fudhail Afwn <fudhailx2004@gmail.com>
2024-06-09 15:20:57 +0800

    Modify by Fudhail

---
+++ volunteer.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Volunteer Mission</title>
+    <link rel="stylesheet" href="CssFiles/volunteer.css">
+</head>
+<body>
+
+    <div class="Container">
+        <div class="FirstPageContainer">
+            <div class="section-text">
+                <div class="RowOne">
+                    <h1>Join a dedicated</h1>
+                    <h1>team fueling a meaningful</h1>
+                    <h1>mission</h1>
+                </div>
+                <div class="RowTwo">
+                    <p>Help us tackle poverty in Malacca by providing resources and raising </p>
+                    <p>awareness. Volunteer to support those in need.</p>
+                </div>
+            </div>
+            <div class="section-image">
+                <img src="/images/BukitBintang.jpg" alt="Volunteer pictures">
+            </div>
+        </div>
+    </div>
+
+    <!-- Section Statistics -->
+    <div class="Container-statistic">
+        <div class="SecondPageContainer">
+            <div class="statisticvolunteer">
+                <div class="RowOne">
+                    <h1>Making an impact through volunteering</h1>
+                    <p>We are on a mission to empower volunteers of all levels to make a difference in </p>
+                    <p>fighting poverty. By joining us, you can contribute to meaningful impact and</p>
+                    <p>support those in need, enabling our community to thrive.</p>
+                </div>
+            <div class="RowThree">
+                <p>Powering</p>
+                <h1>200</h1>
+                <p>Volunteers</p>
+            </div>
+            
+            <div class="RowFour">
+                <p>Powering</p>
+                <h1>70</h1>
+                <p>Places</p>
+            </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Opportunities Volunteer -->
+    <div class="Container-opportunities">
+        <div class="ThirdPageContainer">
+            <div class="opportunitiesvolunteer">
+                    <div class="RowOne">
+                        <h1>Join us to unlock opportunities in fighting poverty. </h1>
+                        <p>Welcome to OneCare, where we're dedicated to making a difference in Melaka. Join us in empowering our community through volunteering and</p>
+                        <p>support. Together, we can create positive change.</p>
+                        
+                    </div>
+
+
+                    <!-- Search -->
+                    <div class="search-container">
+                        <input type="text" placeholder="Find your volunteering district" class="search-input">
+                        <button class="search-button">Find</button>
+                    </div>
+
+                    <div class="places-container">
+                        <div class="places">
+                            <img src="/images/kampung1.jpg" alt="kampungsatu">
+                            <h3>Kampung Haji Anuar</h3>
+                            <p><strong>Main Requirement</strong></p>
+                            <ul>
+                                <li>Need Volunteering Energy</li>
+                                <li>Need Cleaning Tools</li>
+                            </ul>
+                            <p><strong>Address:</strong> Jalan Haji Anuar 89, Ayer Keroh, Melaka</p>
+                            <p><strong>On Duty:</strong> 12/09/2024</p>
+                            <button>Join</button>
+                        </div>
+
+                        <div class="places">
+                            <img src="/images/kampung2.jpg" alt="kampungdua">
+                            <h3>Kampung Haji Abdul</h3>
+                            <p><strong>Main Requirement</strong></p>
+                            <ul>
+                                <li> - Need Volunteering Energy</li>
+                                <li> - Need Cleaning Tools</li>
+                            </ul>
+                            <p><strong>Address:</strong> Jalan Haji Abdul 89, Ayer Keroh, Melaka</p>
+                            <p><strong>On Duty:</strong> 12/08/2024</p>
+                            <button>Join</button>
+                        </div>
+
+                        <div class="places">
+                            <img src="/images/kampung3.jpg" alt="kampungtiga">
+                            <h3>Kampung Ali Rostam</h3>
+                            <p><strong>Main Requirement</strong></p>
+                            <ul>
+                                <li>Need Volunteering Energy</li>
+                                <li>Need Cleaning Tools</li>
+                            </ul>
+                            <p><strong>Address:</strong> Jalan Ali Rostam 47, Bukit Baru, Melaka</p>
+                            <p><strong>On Duty:</strong> 30/08/2024</p>
+                            <button>Join</button>
+                        </div>
+
+                    </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="Container-feedback">
+        <div class="FourthPageContainer">
+            <div class="userfeedback">
+                <div class="Border">
+                    <h1>Tell us what you think!</h1>
+                    <p>We value knowledge sharing and acrive participation. Share your feedback with us to help us make a meaningful impact </p>
+                    <p>through the OneCare Volunteering initiative.</p>
+                    <button>Learn More</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
