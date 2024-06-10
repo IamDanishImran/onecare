d46eb00fb11373243c49ef271ed6225537bedaad
Danish Imran <IamDanishImran@users.noreply.github.com>
2024-06-10 14:01:26 +0800

    Add by Imran

---
+++ newsroom.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>OneCare</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CssFiles/newsroom.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  </head>
+  <body>
+  <section class="main">
+        <!-- Header Section -->
+        <?php include 'header.php'; ?>
+
+        <!-- Intro Section -->
+        <article class="intro-Content">
+            <section class="intro">
+                <h1>Empowering People to <br> change the <span id="introTitle">World</span></h1>
+                <br>
+                <p>This is temporary text in a single paragraph. Perhaps it will be
+                    change soon before we launch the websites, thank you. 
+                </p>
+            </section>
+            <section class="introImage">
+                <img src="media/news.png" alt="Intro-illustration">
+            </section>
+        </article>
+    </section>
+
+        <!-- Footer Section -->
+        <?php include 'footer.php'; ?>
+</body>
+<html>
